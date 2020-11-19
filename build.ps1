9d1cc593243d0ebc02fc9eab5d7eae13a9356624
Camto <www.ben.pr@gmail.com>
2020-11-18 22:28:27 -0400

    Don't use alias.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-rmdir -R ./src/js
+Remove-Item -R ./src/js
 $ts_files = @(Get-ChildItem "./src/*.ts")
 tsc --outDir ./src/js --module commonjs --moduleResolution node @ts_files
 browserify ./src/js/main.js -s main -o ./web/main.js
