6077241e0fea80426042e9cfd68be147c3c9eb95
Camto <www.ben.pr@gmail.com>
2020-12-25 02:29:13 -0400

    Fix up old `types.ts`

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,14 @@
-let types = [
-	"op", "name", "ref", "obj", "prop", "ls", "num", "str", // Basic
-	"if", "for", "while", "var", "fun", // Structures
-	"cmmnt"
-];
-
-function type(name) {
-	return types.indexOf(name);
+enum Node_Type {
+	op, name, ref, obj, prop, ls, num, str, // Basic
+	if, for, while, var, fun, // Structures
+	cmmnt
 }
 
-let ops = [
+enum Op {
 	"==", "/=", "<=", ">=", "<", ">", // Comparison
 	"+", "~", "-", "*", "/", // Arithmetic
 	"^", "%%", "%", "'", // Extra math
 	"@", // List indexing
 	"&", "|", "!", // Boolean
 	"??" // Special interaction
-];
+}
